e4c5e53904eecfd7da45da6162f5777a543e5c82
IxLahiji <ixabat16@gmail.com>
2017-05-01 16:31:29 -0600

    Created skeleton code for algorithm

---
+++ chatBot/profile.py
@@ -0,0 +1,14 @@
+
+
+class TextProfile:
+    
+    def __init__(self, text_list):
+        pass
+        
+    
+    def generate_profile():
+        pass
+    
+    
+    def generate_sentence():
+        pass
