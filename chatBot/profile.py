e33cedbe5ebcb39fe73345221fc191fbe74d8dd3
IxLahiji <ixabat16@gmail.com>
2017-05-01 18:17:33 -0600

    Implemented profile generation

--- chatBot/profile.py
+++ chatBot/profile.py
@@ -1,14 +1,39 @@
-
+import itertools
+import numpy
 
 class TextProfile:
     
     def __init__(self, text_list):
-        pass
+        self.source_list = text_list
+        self.generate_profile()
         
     
-    def generate_profile():
-        pass
+    def generate_profile(self):
+        new_profile = {}
+        new_sentence_init_profile = {}
+        
+        for sentence in self.source_list:
+            sentence_list = sentence.split()
+            
+            first_word = sentence_list[0].lower()
+            if (first_word not in new_sentence_init_profile):
+                    new_sentence_init_profile[first_word] = 0
+            new_sentence_init_profile[first_word] += 1
+            
+            for curr_word, next_word in itertools.zip_longest(sentence_list, sentence_list[1:]):
+                curr_word = curr_word.lower()
+                next_word = next_word.lower()
+                
+                if (curr_word not in new_profile):
+                    new_profile[curr_word] = {}
+                
+                if (next_word not in new_profile[curr_word]):
+                    new_profile[curr_word][next_word] = 0
+                
+                new_profile[curr_word][next_word] += 1
+        
+        self.sentence_init_profile = new_sentence_init_profile
+        self.profile = new_profile
     
-    
-    def generate_sentence():
+    def generate_sentence(self):
         pass
